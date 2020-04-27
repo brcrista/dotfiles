319f1c5a1ac9eb251a8f2b016264c70ef1907c83
Brian Cristante <brian.cristante@gmail.com>
2020-04-26 21:27:26 -0400

    Install utilities for macOS

---
+++ brew.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Install command-line tools for macOS using Homebrew.
+
+brew update
+brew upgrade
+
+# Install GNU versions of Unix tools (and some missing ones)
+brew install coreutils
+brew install findutils
+brew install gnu-sed
+brew install bash
+brew install autoconf
+brew install automake
+
+coreutils_path="/usr/local/opt/coreutils/libexec/gnubin"
+PATH="$coreutils_path:$PATH"
+pushd $coreutils_path && ln -s ln -s ../../bin/gsed sed && popd
+# TODO How can I route `man sed` to `man gsed`?
+
+# Install scripting languages
+brew cask install miniconda
+brew install node
+
+# Install other shell programs
+brew install jq
+brew install tree
+npm install -g tldr
+
+brew cleanup
+
+export PATH
---
+++ macos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install packages from Homebrew
+bash brew.sh
