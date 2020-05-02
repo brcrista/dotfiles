315259cf0aee28518e9b67e6e739cd82b9c9bdaf
Brian Cristante <brcrista@github.com>
2020-05-01 21:26:34 -0400

    Add Python setup for macOS

--- macos.sh
+++ macos.sh
@@ -5,4 +5,7 @@ set -e
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install packages from Homebrew
-bash brew.sh
+bash macos/brew.sh
+
+# Set up Miniconda
+bash macos/conda.sh
---
+++ macos/conda.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# Download conda
+install_script="$HOME/Downloads/install_miniconda.sh"
+curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o "$install_script"
+chmod +x "$install_script"
+
+# Install conda
+conda_prefix="$HOME/miniconda"
+"$install_script" -b -p "$conda_prefix"
+rm "$install_script"
+
+# Install Anaconda packages
+PATH="$conda_prefix/condabin:$PATH"
+conda install python --yes
+conda install --file conda-environment.yml --yes
+
+# Add conda and packages to PATH
+echo "PATH=$conda_prefix/bin:$conda_prefix/condabin:\$PATH" >> ~/.bash_profile.plugins
